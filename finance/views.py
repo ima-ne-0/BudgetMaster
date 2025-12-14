e52347eb00e7650d2552e6c6f5430bdb1cab1bce
ima_ne <dibtimane@gmail.com>
2025-12-14 23:11:34 +0100

    Add viewsets for Category and Transaction models

--- finance/views.py
+++ finance/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Category, Transaction
+from .serializers import CategorySerializer, TransactionSerializer
 
-# Create your views here.
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+class TransactionViewSet(viewsets.ModelViewSet):
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionSerializer
