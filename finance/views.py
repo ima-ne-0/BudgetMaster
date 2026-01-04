224e3f8ace4d3b61a132a9c81e7510a22cc5b241
ima_ne <dibtimane@gmail.com>
2026-01-04 17:09:20 +0100

    Implement summary action in TransactionViewSet
    Add summary action to TransactionViewSet for income and expense calculations.

--- finance/views.py
+++ finance/views.py
@@ -1,4 +1,7 @@
 from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.db.models import Sum
 from .models import Category, Transaction
 from .serializers import CategorySerializer, TransactionSerializer
 
@@ -9,3 +12,17 @@ class CategoryViewSet(viewsets.ModelViewSet):
 class TransactionViewSet(viewsets.ModelViewSet):
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
+
+    # C'est la partie "Magique" pour ta démo
+    @action(detail=False, methods=['get'])
+    def summary(self, request):
+        # Calcule la somme des revenus
+        income = Transaction.objects.filter(category__type='INCOME').aggregate(Sum('amount'))['amount__sum'] or 0
+        # Calcule la somme des dépenses
+        expense = Transaction.objects.filter(category__type='EXPENSE').aggregate(Sum('amount'))['amount__sum'] or 0
+        
+        return Response({
+            "total_income": income,
+            "total_expense": expense,
+            "balance": income - expense
+        })
